3c4c1bc1c74901ce81a64193026170a41f179e9f
Guillem <guillem.mart7@gmail.com>
2022-04-25 19:12:09 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -79,24 +79,26 @@ class War {
       const result = this.vikingArmy[this.vikingIndex].receiveDamage(this.saxonArmy[this.saxonIndex].strength);
       if(this.vikingArmy[this.vikingIndex].health <= 0) {
         this.vikingArmy.splice(this.vikingIndex,1);
-      } else if(randomAttack === 1) {
-        const vikingIndex = Math.floor(Math.random()*this.vikingArmy.length); 
-        const saxonIndex = Math.floor(Math.random()*this.saxonArmy.length);
-        const result = this.saxonArmy[this.saxonIndex].receiveDamage(this.vikingArmy[this.vikingIndex].strength);
-        if(this.saxonArmy[this.saxonIndex].health <= 0) {
-          this.saxonArmy.splice(saxonIndex,1);
-        } 
+      }
+    } else if(randomAttack === 1) {
+      const vikingIndex = Math.floor(Math.random()*this.vikingArmy.length); 
+      const saxonIndex = Math.floor(Math.random()*this.saxonArmy.length);
+      const result = this.saxonArmy[this.saxonIndex].receiveDamage(this.vikingArmy[this.vikingIndex].strength);
+      if(this.saxonArmy[this.saxonIndex].health <= 0) {
+        this.saxonArmy.splice(saxonIndex,1);
       }
     } return result;
-  }
+  } 
+
   showStatus() {
     if(this.saxonArmy.length === 0) {
       return "Saxons have won the war of the century!"
     } else if (this.vikingArmy.length === 0) {
       return "Vikings have won the war of the century!"
-    } else if (this.vikingArmy.length === 1) 
+    } else if (this.vikingArmy.length === 1) {
+      return "Vikings and Saxons are still in the thick of battle."
+    }
   }
-
 }
 
 
