b7dfc5570a73a12cf07b62e0945ec05d6d2d5724
Guillem <guillem.mart7@gmail.com>
2022-04-25 19:15:33 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -92,10 +92,10 @@ class War {
 
   showStatus() {
     if(this.saxonArmy.length === 0) {
-      return "Saxons have won the war of the century!"
-    } else if (this.vikingArmy.length === 0) {
       return "Vikings have won the war of the century!"
-    } else if (this.vikingArmy.length === 1) {
+    } else if (this.vikingArmy.length === 0) {
+      return "Saxons have won the war of the century!"
+    } else {
       return "Vikings and Saxons are still in the thick of battle."
     }
   }
